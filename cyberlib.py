c1cb3247b5080656abf36476923a0ad1983c3841
Luzog78 <luzog78@gmail.com>
2024-10-21 09:35:20 +0200

    + | ArgParser (cyberlib.py) ~ | Makefiles (abspath -> which)

--- cyberlib.py
+++ cyberlib.py
@@ -1,3 +1,15 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    cyberlib.py                                        :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: ysabik <ysabik@student.42.fr>              +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2024/10/21 09:31:07 by ysabik            #+#    #+#              #
+#    Updated: 2024/10/21 09:33:49 by ysabik           ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
 class Color:
 	BLACK		= '\033[30m'
 	RED			= '\033[31m'
@@ -148,3 +160,236 @@ def cprint(
 		char: str = Color.DEFAULT_MAGIC_CHAR,
 		) -> None:
 	Color.print(*values, sep=sep, end=end, reset=reset, char=char)
+
+
+class ArgError(Exception):
+	pass
+
+
+class ArgFlag:
+	def __init__(self, short: str | None, long: str | None,
+			args: list[str] | None, desc: str | None) -> None:
+		self.short: str | None			= short
+		self.long: str | None			= long
+		self.args: list[str] | None		= args
+		self.desc: str | None			= desc
+		self.values: list[str]			= []
+
+		self.combinable: bool	= len(self.short) == 1 if self.short else False
+		self.args_len: int	= sum(len(arg) + 3 for arg in args) if args else 0
+		self.short_len: int	= len(short) + 1 + self.args_len if short else 0
+		self.long_len: int	= len(long) + 2 + self.args_len if long else 0
+	
+	def clear(self) -> None:
+		self.values.clear()
+	
+	def __str__(self, short_len: int = 0, long_len: int = 0) -> str:
+		short = ''
+		if self.short:
+			f = '{:<' + str(short_len + (2 if self.long else 0)) + '}'
+			comma = ', ' if self.long else ''
+			if self.args:
+				short = f.format(f'-{self.short} <{self.args[0]}>{comma}')
+			else:
+				short = f.format(f'-{self.short}{comma}')
+		long = ''
+		if self.long:
+			if not self.short:
+				short += ' ' * (short_len + 2)
+			f = '{:<' + str(long_len) + '}'
+			if self.args:
+				long = f.format(f'--{self.long} <{self.args[0]}>')
+			else:
+				long = f.format(f'--{self.long}')
+		elif long_len:
+			long = ' ' * (long_len + (2 if short else 0))
+		return f'{short}{long}  {self.desc}'
+	
+	def __repr__(self) -> str:
+		return '<ArgFlag' \
+			+ f' short={repr(self.short)}' \
+			+ f' long={repr(self.long)}' \
+			+ f' args={self.args}' \
+			+ f' desc={repr(self.desc)}' \
+			+ f' combinable={self.combinable}' \
+			+ f' args_len={self.args_len}' \
+			+ f' short_len={self.short_len}' \
+			+ f' long_len={self.long_len}' \
+			+ f' values={self.values}' \
+			+ '>'
+
+
+class ArgParser:
+	def __init__(self, args: list[str], case_sensitive: bool = True) -> None:
+		'''Create a new ArgParser object
+
+		:param args: The arguments to parse (usually `sys.argv[1:]` --> `sys.argv` without the script name)
+		:type args: list[str]
+		:param case_sensitive: Whether the parser should be case sensitive or not ONLY for flags
+		:type case_sensitive: bool
+
+		:Example: ::
+			ArgParser(sys.argv)
+				.add_flag('v', 'verbose', None, 'Print more information')
+				.add_flag('q', 'quiet', None, 'Print less information')
+				.parse()
+
+			# If the script is called with `./script.py -v abc --quiet def`
+			# The flags will be:
+			#   - verbose: ['0']
+			#   - quiet: ['2']
+			# And the free_args will be: ['abc', 'def']
+		'''
+		self.args: list[str]		= args
+		self.case_sensitive: bool	= case_sensitive
+		self.flags: set[ArgFlag]	= set()
+		self.free_args: list[str]	= []
+
+	def add_flag(self, short: str | None, long: str | None,
+			args: list[str] | None, desc: str | None) -> 'ArgParser':
+		'''Add a flag (new instance of :class:`ArgFlag`) to the parser
+
+		:param short: The short name of the flag (not including the '-')
+		:type short: str
+		:param long: The long name of the flag (not including the '--')
+		:type long: str
+		:param args: The arguments the flag requires
+		:type args: list[str]
+		:param desc: The description of the flag
+		:type desc: str
+
+		:return: The current ArgParser object
+		:rtype: ArgParser
+		'''
+		self.flags.add(ArgFlag(short, long, args, desc))
+		return self
+
+	def parse(self) -> 'ArgParser':
+		'''Parse the arguments and fill the flags and free_args lists
+
+		:raises ArgError: If an unknown flag is found or if a flag requires more arguments
+
+		:return: The current ArgParser object
+		:rtype: ArgParser
+		'''
+		self.free_args.clear()
+		(flag.clear() for flag in self.flags)
+		i = 0
+		free_only = False
+		while i < len(self.args):
+			arg = self.args[i] if self.case_sensitive else self.args[i].lower()
+			if not free_only and arg == '--':
+				free_only = True
+			elif not free_only and arg.startswith('-') and arg != '-':
+				for flag in self.flags:
+					short, long = f'-{flag.short}', f'--{flag.long}'
+					if not self.case_sensitive:
+						short, long = short.lower(), long.lower()
+					if arg == short or arg == long:
+						if flag.args:
+							flag.values.clear()
+							i += 1
+							while len(flag.values) < len(flag.args) \
+									and i < len(self.args) \
+									and not self.args[i].startswith('-'):
+								flag.values.append(self.args[i])
+								i += 1
+							i -= 1
+							if len(flag.values) != len(flag.args):
+								raise ArgError(f'Not enough arguments for flag {arg}')
+						else:
+							flag.values.append(f'{i}')
+						break
+				else:
+					if arg[1] == '-':
+						raise ArgError(f'Unknown flag {arg}')
+					for j, c in enumerate(arg[1:]):
+						for flag in self.flags:
+							if not flag.combinable:
+								continue
+							sh = flag.short if self.case_sensitive else flag.short.lower()
+							if c == sh:
+								if flag.args:
+									flag.values.clear()
+									i += 1
+									while len(flag.values) < len(flag.args) \
+											and i < len(self.args) \
+											and not self.args[i].startswith('-'):
+										flag.values.append(self.args[i])
+										i += 1
+									i -= 1
+									if len(flag.values) != len(flag.args):
+										raise ArgError(f'Not enough arguments for flag -{c} (at position {j} in {arg})')
+								else:
+									flag.values.append(f'{i}.{j}')
+								break
+						else:
+							raise ArgError(f'Unknown flag -{c} (at position {j} in {arg})')
+			else:
+				self.free_args.append(self.args[i])
+			i += 1
+		return self
+
+	def get_flag(self, identifier: int | str) -> ArgFlag | None:
+		'''Get a flag by its identifier
+
+		:param identifier: The identifier of the flag (either the index, the short or the long name)
+		:type identifier: int | str
+
+		:return: The flag if found, None otherwise
+		:rtype: ArgFlag | None
+		'''
+		if isinstance(identifier, int):
+			if identifier >= 0 and identifier < len(self.flags):
+				return list(self.flags)[identifier]
+		identifier = str(identifier)
+		for flag in self.flags:
+			short, long = f'-{flag.short}', f'--{flag.long}'
+			if not self.case_sensitive:
+				short, long = short.lower(), long.lower()
+			if identifier == short or identifier == long:
+				return flag
+		for flag in self.flags:
+			short, long = flag.short, flag.long
+			if not self.case_sensitive:
+				short, long = short.lower(), long.lower()
+			if identifier == short or identifier == long:
+				return flag
+		return None
+
+	def __str__(self) -> str:
+		'''Return a string representation of the ArgParser object
+
+		:return: The string representation of the ArgParser object
+		:rtype: str
+
+		:Example: ::
+			parser = ArgParser(sys.argv)
+				.add_flag('v',  'verbose', None,    'Print more information')
+				.add_flag('q',  'quiet',   None,    'Print less information')
+				.add_flag(None, 'foo',     ['foo'], 'A foo flag')
+				.add_flag('b',  None,      ['bar'], 'A bar flag')
+				.add_flag('q',  'quux',    ['fb'],  'A quux flag')
+				.parse()
+			print(parser)
+
+			# Output:
+			# Flags:
+			#             --foo <foo>  A foo flag
+			#   -v,       --verbose    Print more information
+			#   -x <fb>,  --quux <fb>  A quux flag
+			#   -q,       --quiet      Print less information
+			#   -b <bar>               A bar flag
+		'''
+		short_len = max((flag.short_len for flag in self.flags), default=0)
+		long_len = max((flag.long_len for flag in self.flags), default=0)
+		lines = '\n'.join('  ' + flag.__str__(short_len, long_len) for flag in self.flags)
+		return 'Flags:\n' + lines
+	
+	def __repr__(self) -> str:
+		return '<ArgParser' \
+			+ f' args={self.args}' \
+			+ f' case_sensitive={self.case_sensitive}' \
+			+ f' free_args={self.free_args}' \
+			+ f' flags={self.flags}' \
+			+ '>'
